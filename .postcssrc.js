874ef55b0f119006a16f2ec598184fa4d7ef7468
wittyMouse <505357658@qq.com>
2020-01-21 01:03:43 +0800

    增加postcss插件postcss-px-to-viewport和postcss配置

---
+++ .postcssrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    'autoprefixer': {
+      // browsers: ['Android >= 4.0', 'iOS >= 8']
+    },
+    'postcss-px-to-viewport': {
+      unitToConvert: 'px',
+      viewportWidth: 750,
+      unitPrecision: 5,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: [],
+      landscape: false,
+      landscapeUnit: 'vw',
+      landscapeWidth: 1334
+    }
+  }
+}
