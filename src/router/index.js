51bd6eb6b391395e27ed3a795ed758c52d58565c
wittyMouse <505357658@qq.com>
2020-01-21 01:02:35 +0800

    更新路由 1.增加全局前置路由（修改网页标题） 2.懒加载非首次访问页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from '../views/HomePage.vue'
-import GetRedWine from '../views/GetRedWine.vue'
-import PayResultSuccess from '../views/PayResultSuccess.vue'
-import PayResultFailure from '../views/PayResultFailure.vue'
 
 Vue.use(VueRouter)
 
@@ -11,35 +8,65 @@ const routes = [
   {
     path: '/',
     name: 'home-page',
-    component: HomePage
+    component: HomePage,
+    meta: { title: '恭喜获得￥899进口红酒1瓶' }
   },
   {
     path: '/getRedWine',
     name: 'getRedWine',
-    component: GetRedWine
+    component: () => import(/* webpackChunkName: "GetRedWine" */ '../views/GetRedWine.vue'),
+    meta: { title: '领取进口红酒' }
   },
   {
     path: '/successPage',
     name: 'success-page',
-    component: PayResultSuccess
+    component: () => import(/* webpackChunkName: "PayResultSuccess" */ '../views/PayResultSuccess.vue'),
+    meta: { title: '订单详情' }
   },
   {
     path: '/failurePage',
     name: 'failure-page',
-    component: PayResultFailure
+    component: () => import(/* webpackChunkName: "PayResultFailure" */ '../views/PayResultFailure.vue'),
+    meta: { title: '订单详情' }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    redirect: '/'
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   route level code-splitting
+  //   this generates a separate chunk (about.[hash].js) for this route
+  //   which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 全局前置路由
+router.beforeEach((to, from, next) => {
+  // if (to.matched.some(record => record.meta.requiresAuth)) {
+  //   // this route requires auth, check if logged in
+  //   // if not, redirect to login page.
+  //   if (!auth.loggedIn()) {
+  //     next({
+  //       path: '/login',
+  //       query: { redirect: to.fullPath }
+  //     })
+  //   } else {
+  //     next()
+  //   }
+  // } else {
+  //   next() // 确保一定要调用 next()
+  // }
+  if (document.title !== to.meta.title) {
+    document.title = to.meta.title || ''
+  }
+  next()
 })
 
 export default router
