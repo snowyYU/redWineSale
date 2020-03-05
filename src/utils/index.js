a353d2fb1e5a9fe2bcce8eecc79eb7abdc6292e5
wittyMouse <505357658@qq.com>
2020-03-05 22:50:44 +0800

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -226,7 +226,7 @@ export function getUrl () {
  */
 export function wxConfig (appId, timestamp, nonceStr, signature) {
   wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId, // 必填，公众号的唯一标识
     timestamp,
     nonceStr,
