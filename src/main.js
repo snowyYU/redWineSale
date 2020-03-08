09ef9cdaa20860aed7ea0d9f5a94e40e98fa79f0
wittyMouse <505357658@qq.com>
2020-03-08 21:03:28 +0800

    update

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import './assets/style.scss'
 import { Button, Popup, Progress, Cell, CellGroup, Field, AddressEdit, Toast, Area, Loading, Image, SubmitBar, NumberKeyboard, RadioGroup, Radio, Icon, Lazyload, Overlay } from 'vant'
 // 复制到剪贴板
 import VueClipboard from 'vue-clipboard2'
-import vconsole from 'vconsole'
+// import vconsole from 'vconsole'
 
-new vconsole()
+// new vconsole()
 Vue.use(VueClipboard)
 Vue.use(Button).use(Popup).use(Progress).use(Cell).use(CellGroup).use(Field).use(AddressEdit).use(Toast).use(Area).use(Loading).use(Image).use(SubmitBar).use(NumberKeyboard).use(RadioGroup).use(Radio).use(Icon).use(Lazyload).use(Overlay)
 Vue.config.productionTip = false
